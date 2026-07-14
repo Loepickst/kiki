a0a53f9b3a31ed75a7706a6fd596ef931a63e983
Loepickst <kiki418255199@gmail.com>
2026-07-14 19:43:27 +0800

    Add files via upload

---
+++ daily/light-read/daily/assets/vocab-pronunciations.js
@@ -0,0 +1,6 @@
+window.KikiReadingPronunciations = Object.freeze({
+    provider: "microsoft-azure-speech",
+    voice: "ja-JP-NanamiNeural",
+    generatedAt: "",
+    entries: Object.freeze({})
+});
